d72511e69c70ba7892deb0938f310f14db510a53
Cyalisonliu <alison610310@gmail.com>
2026-09-12 20:31:20 -0400

    feat: add screen clock functionality and add demo image

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,4 +1,4 @@
-import time
+from time import strftime, sleep
 import subprocess
 import digitalio
 import board
@@ -62,10 +62,11 @@ backlight.value = True
 
 while True:
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=400)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    current_time = strftime("%m/%d/%Y %H:%M:%S")
+    draw.text((width//2, height//2), current_time, font=font, fill="#FFFFFF", anchor="mm")
 
     # Display image.
     disp.image(image, rotation)
-    time.sleep(1)
+    sleep(1)
